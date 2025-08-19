b222fe0c8770bef169394d7af3882a0dcd220940
matt <matteovanin321@gmail.com>
2025-08-19 14:33:40 -0300

    Contador de cliques

---
+++ app/src/main/java/app/contadordecliques/MainActivity.java
@@ -0,0 +1,32 @@
+package app.contadordecliques;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    int i = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textview);
+        tv.setText(String.valueOf(i));
+        tv.setOnClickListener(v -> {
+            i++;
+            tv.setText(String.valueOf(i));
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

faca78da7c64a998f71130151c16f99edc413255
Matt <matteovanin321@gmail.com>
2025-08-20 20:19:34 -0300

    ciclo de vida activity

--- app/src/main/java/rbenica/app/MainActivity.java
+++ app/src/main/java/rbenica/app/MainActivity.java
@@ -1,6 +1,7 @@
 package rbenica.app;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +14,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        getString(R.string.hello);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d("ciclo_vida", "Oncreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ciclo_vida", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ciclo_vida", "OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ciclo_vida", "OnPause");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("ciclo_vida", "onDestroy");
     }
 }

1238b164959c7ff02300308baf4f5fb672e2cab1
Matt <matteovanin321@gmail.com>
2025-08-20 22:29:50 -0300

    Sorteio

--- app/src/main/java/rbenica/app/MainActivity.java
+++ app/src/main/java/rbenica/app/MainActivity.java
@@ -1,6 +1,9 @@
 package rbenica.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +11,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    TextView textViewResult;
+    EditText editTextMin, editTextMax;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        getString(R.string.hello);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        editTextMin=findViewById(R.id.edMin);
+        editTextMax=findViewById(R.id.edMax2);
+        textViewResult=findViewById(R.id.textviewresult);
+
+        button.setOnClickListener(v -> {
+            int min=Integer.parseInt( editTextMin.getText().toString() );
+            int max=Integer.parseInt( editTextMax.getText().toString() );
+            int sorteado=0;
+            Random random = new Random();
+            sorteado = (int) (Math.random()*(max-min)+min);
+
+            textViewResult.setText(Integer.toString(sorteado));
+                });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

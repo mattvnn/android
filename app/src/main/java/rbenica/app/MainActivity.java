41eadabf5d8af50a5006a877824fe618b6b566d9
Matt <matteovanin321@gmail.com>
2025-09-03 21:16:08 -0300

    onSaveInstanceState e onRestoreInstanceState

--- app/src/main/java/rbenica/app/MainActivity.java
+++ app/src/main/java/rbenica/app/MainActivity.java
@@ -1,20 +1,19 @@
 package rbenica.app;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textViewResult;
+    TextView tv;
     EditText editTextMin, editTextMax;
     Button button;
 
@@ -23,24 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
-        editTextMin=findViewById(R.id.edMin);
-        editTextMax=findViewById(R.id.edMax2);
-        textViewResult=findViewById(R.id.textviewresult);
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax2);
+        tv = findViewById(R.id.tv);
 
         button.setOnClickListener(v -> {
-            int min=Integer.parseInt( editTextMin.getText().toString() );
-            int max=Integer.parseInt( editTextMax.getText().toString() );
-            int sorteado=0;
+            int min = Integer.parseInt(editTextMin.getText().toString());
+            int max = Integer.parseInt(editTextMax.getText().toString());
+            int sorteado = 0;
             Random random = new Random();
-            sorteado = (int) (Math.random()*(max-min)+min);
+            sorteado = (int) (Math.random() * (max - min) + min);
 
-            textViewResult.setText(Integer.toString(sorteado));
-                });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            tv.setText(Integer.toString(sorteado));
         });
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("Sorteado", tv.getText().toString());
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState!=null) {
+            tv.setText(savedInstanceState.getString("Sorteado"));
+        }
+    }
 }
+
+
